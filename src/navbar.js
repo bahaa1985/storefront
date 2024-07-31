08421b80b7cff791c2266f61e6689449b74da8ad
bahaa1985 <bahaa_salaheldin@live.com>
2024-07-31 23:01:34 +0300

    gsap navbar test1

--- src/navbar.js
+++ src/navbar.js
@@ -2,26 +2,49 @@ const hamburger = document.getElementById("hamburger_button");
 const hamburger_items = document.getElementById("hamburger_items");
 const hamburger_lines = document.getElementById("hamburger_lines");
 const hamburger_close = document.getElementById("hamburger_close");
-const bell = document.getElementById("bell");
+const myul=document.querySelector('.myul');
+// const bell = document.getElementById("bell");
 const search = document.getElementById("search");
-const notifications = document.getElementById("notifications");
-const notifications_count = document.getElementById("notifications_count");
+// const notifications = document.getElementById("notifications");
+// const notifications_count = document.getElementById("notifications_count");
 const NAV_BAR = document.getElementById("nav_bar");
-
+const lis=document.querySelectorAll('.liclass');
 //Hamburger button click event handler to show or hide it:
+let expanded=false;
+let tween = gsap.to(myul,{
+	height:'auto',duration:0.5,delay:-0.5	
+})
+let tween2=gsap.to(lis,{
+	opacity:'100%',stagger:0.1
+})
+tween.pause();tween2.pause();
 hamburger.addEventListener("click", () => {
-	hamburger_items.classList.toggle("hidden");
+	// hamburger_items.classList.toggle("hidden");
 	hamburger_lines.classList.toggle("hidden");
 	hamburger_close.classList.toggle("hidden");
-	notifications.classList.add("hidden");
+	// notifications.classList.add("hidden");
+	
+	//gsap animations:
+	if(!expanded){
+		tween.play();
+		tween2.play();
+		expanded=true;
+		console.log(tween2.progress);
+	}
+	else{		
+		tween.reverse();
+		tween2.reverse();				
+		expanded=false;
+		console.log(tween2.progress);
+	}
 });
 // to show to hide notifications when clicking bell icon:
-bell.addEventListener("click", () => {
-	bell.setAttribute("fill", "#fff");
-	bell.classList.remove("bell_ring");
-	notifications.classList.toggle("hidden");
-	notifications_count.classList.add("hidden");
-});
+// bell.addEventListener("click", () => {
+	// bell.setAttribute("fill", "#fff");
+	// bell.classList.remove("bell_ring");
+	// notifications.classList.toggle("hidden");
+	// notifications_count.classList.add("hidden");
+// });
 //test
 //dark_mode:
 const body = document.getElementsByTagName("body")[0];
